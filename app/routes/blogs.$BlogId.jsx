9bd93b673986f0a3d289e4a6645bb53a88cd1c33
priyanshu082 <priyanshusingh216@gmail.com>
2024-06-02 09:32:37 +0530

    form state is done

--- app/routes/blogs.$BlogId.jsx
+++ app/routes/blogs.$BlogId.jsx
@@ -1,5 +1,5 @@
 import { json } from "@remix-run/node";
-import { Form, Link, useLoaderData } from "@remix-run/react";
+import { Form, Link, useLoaderData, useNavigate, useNavigation } from "@remix-run/react";
 
 export const loader = async ({ params }) => {
   const res = await fetch(
@@ -7,32 +7,35 @@ export const loader = async ({ params }) => {
   );
 
   const data = await res.json();
-  
-//   console.log(data);
   return json({ blog: data });
 };
 
-export const action=async({request,params})=>{
-    const formData=await request.formData();
-    const title=formData.get("title")
-    console.log(title,params)
+export const action = async ({ request, params }) => {
+  const formData = await request.formData();
+  const title = formData.get("title");
+  console.log(title, params);
 
-    const res = await fetch(
-        `https://jsonplaceholder.typicode.com/posts/${params.BlogId}`,
-        {
-            body:JSON.stringify({title}),
-            method:'PATCH',
-        }
-      );
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.BlogId}`,
+    {
+      body: JSON.stringify({ title }),
+      method: "PATCH",
+    }
+  );
 
-      const data=await res.json()
-console.log(data)
-    return json({post:data})
+  const data = await res.json();
+  console.log(data);
+  return json({ post: data });
+};
 
-}
 
+  
 const Blogs = () => {
   const { blog } = useLoaderData();
+
+  const navigation=useNavigation()
+
+const isSubmitting=!(navigation.state==='idle')
 
   return (
     <div>
@@ -49,15 +52,16 @@ const Blogs = () => {
       </div>
 
       <Form method="patch">
-      <div className="mt-[20px] bg-yellow-200 p-[10px]">
-        <input name="title" placeholder="title"/>
-        <button type="submit" className="ml-[20px] bg-pink-400 px-[10px] py-[3px] rounded-md">
-            update
-        </button>
-      </div>
+        <div className="mt-[20px] bg-yellow-200 p-[10px]">
+          <input name="title" placeholder="title" />
+          <button
+            type="submit"
+            className="ml-[20px] bg-pink-400 px-[10px] py-[3px] rounded-md"
+          >
+           {isSubmitting ? "submitting..." : "update"}
+          </button>
+        </div>
       </Form>
-
-     
     </div>
   );
 };
